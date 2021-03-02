181f98c35237c4b5760fd477a691c1b4735d729a
Shultz <75248804+Dufresne0@users.noreply.github.com>
2021-03-02 17:57:28 +0300

    Update Begginig1.c

--- Begginig1.c
+++ Begginig1.c
@@ -1,12 +1,42 @@
-#include "stdio.h"
-
-int main(){
+include <stdio.h>
+ 
+ int main(){
+ 	
+ 	char a;
 	
-	printf("Mert \n"
-	"Blg \n"
-	"Developer \n"
-	"Trainings \n");	
-//YorumStrý
-/*TopluYorumStrý*/
+	float b,c,d;
+	
+	printf("Welcome to Calculator...");
+	printf("\n \n \nFirst Number : ");
+	scanf("%f",&b);
+	printf("\n \nChoose the  operation : ");
+	scanf("%s",&a);
+	printf("\n \nSecond Number : ");
+	scanf("%f",&c);
+	
+	
+	if(a=='+'){
+		d=b+c;
+		printf("Result of the opreation : %f",d);
+	}
+ 	
+ 	else if(a=='-'){
+ 		d=b-c;
+ 		printf("Result of the operation : %f",d); 		
+	 }
+ 	
+ 	else if(a=='/'){
+ 		d=b/c;
+ 		printf("Result of the operation : %f",d);
+	 }
+ 	
+ 	else if(a=='*'){
+ 		d=b*c;
+ 		printf("Result of the opration : %f",d);
+	 }
+	 else{
+	 	printf("You made a wrong choice...");
+	 
+	 }
 	return 0;
-}
+ }
