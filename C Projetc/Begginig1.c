4f9ca8d32a8ffd564ad172e4d822db4eee9a55da
Shultz <75248804+Dufresne0@users.noreply.github.com>
2021-03-08 18:13:37 +0300

    Add files via upload

---
+++ C Projetc/Begginig1.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+int main(){
+	
+	char a;
+	
+	float b,c;
+	
+	printf("Welcome to Calculator...");
+	printf("\n \n \nFirst number : ");
+	scanf("%f",&b);
+	printf("\n \nChoose the  operation(+-/*)");
+	
+
+
+
+
+
+
+
+}
---
+++ C Projetc/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+ 
+  int main(){
+ 	/*WhileLoop*/
+ 	int a=1;
+ 	
+ 	while(a<=50){
+ 		printf("%d\n",a);
+ 		a++;
+ 		printf("FreshlyBakedPie");
+	}
+ 	
+ 	
+ 	
+ 	return 0;
+ }
+ 
