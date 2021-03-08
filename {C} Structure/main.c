fa43cbba28532637d54671df2a6b6872ba82c150
Shultz <75248804+Dufresne0@users.noreply.github.com>
2021-03-08 18:40:55 +0300

    Add files via upload

---
+++ {C} Structure/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+ 
+  int main(){
+ 	/*ForLoop*/
+ 	
+    	int a;
+ 	
+		for(a=1; a<=50; a++){
+			printf("FreshlyBakedPie %d\n",a);	
+		}
+		
+	 
+	 
+	  	
+   		
+	
+ 	return 0;
+ }
+ 
